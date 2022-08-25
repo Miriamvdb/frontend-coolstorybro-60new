352a6b06357243a56e039ebe0423c7168cd8751a
Miriamvdb <miriamvdb@live.nl>
2022-08-25 22:40:31 +0200

    Front/F4: Fetch spacedetails for mySpace

--- src/pages/MySpace/index.js
+++ src/pages/MySpace/index.js
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchSpaceDetails } from "../../store/space/thunks";
 import "./styles.css";
 
 const MySpace = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchSpaceDetails(id));
+  }, [dispatch, id]);
+
   return (
     <div className="container-myspace">
-      <h1>My Space</h1>
+      <h1>My space</h1>
     </div>
   );
 };
--- src/store/user/slice.js
+++ src/store/user/slice.js
@@ -2,7 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   token: localStorage.getItem("token"),
-  profile: null,
+  profile: null, // profile.mySpace is also here!!!
+  mySpace: null, // F4
 };
 
 export const userSlice = createSlice({
@@ -13,14 +14,17 @@ export const userSlice = createSlice({
       localStorage.setItem("token", action.payload.token);
       state.token = action.payload.token;
       state.profile = action.payload.user;
+      state.mySpace = action.payload.mySpace; // F4
     },
     logOut: (state, action) => {
       localStorage.removeItem("token");
       state.token = null;
       state.profile = null;
+      state.mySpace = null; // F4
     },
     tokenStillValid: (state, action) => {
       state.profile = action.payload.user;
+      state.mySpace = action.payload.mySpace; // F4
     },
   },
 });
